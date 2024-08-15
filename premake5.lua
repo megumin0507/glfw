8dd7d84ebefde3b65be474d403e3ae982b527c22
megumin0507 <107980183+megumin0507@users.noreply.github.com>
2024-08-16 06:50:48 +0800

    Add premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,60 @@
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+
+    targetdir ("%{wks.location}/Binaries/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/Intermediate/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/init.c",
+        "src/internal.h",
+        "src/platform.h",
+        "src/platform.c",
+        "src/mappings.h",
+        "src/context.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+
+        "src/null_init.c",
+        "src/null_platform.h",
+        "src/null_joystick.h",
+        "src/null_joystick.c",
+        "src/null_monitor.c",
+        "src/null_window.c"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+
+        files
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.h",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.h",
+            "src/win32_time.c",
+            "src/win32_thread.h",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/win32_module.c",
+            "src/win32_platform.h",
+            "src/wgl_context.c"
+        }
+
+        defines
+        {
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+        }
+        
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
